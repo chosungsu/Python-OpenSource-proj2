b7ad19deece69561d1b0528d98d9bc45f7e558a6
JoSungSu <ski06043@naver.com>
2019-05-06 19:18:18 +0900

    Closes #2

--- Owner.py
+++ Owner.py
@@ -1,5 +1,10 @@
-def Owner(owner):
+def Owner(caller, owner, loc):
     print("This company's owner name is ", owner)
+    print("Our company is located in ", loc) 
+    print(caller, "Welcome to my company!! Wait a minute, please^^")
+    print("===========================================")
 owner = "JoSungSu"
-Owner(owner)
+caller = str(input("What is your name, sir??"))
+loc = "No-Won-Gu"
+Owner(caller, owner, loc)
 print("===========================================")
