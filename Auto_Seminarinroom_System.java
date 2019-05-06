3ff6be31cfed7e22e532d54898dadbaf17b85e89
chosungsu <48382347+chosungsu@users.noreply.github.com>
2019-05-06 16:20:59 +0900

    Delete Auto_Seminarinroom_System.java

--- Auto_Seminarinroom_System.java
+++
@@ -1,8 +0,0 @@
-def Auto_Seminarinroom_System(caller, owner, whenst, room):
-    whenfin = whenst + 2
-    print("Hi,",caller, "Sorry to say you this sad sentences")
-    print(owner, "is in ", room, "room from ", whenst, " to ", whenfin)
-    print("===========================================")
-whenst = random.choice([10, 14, 17, 20])
-room = random.choice([101, 204, 308, 504])
-Auto_Seminarinroom_System(caller, owner, whenst, room)
