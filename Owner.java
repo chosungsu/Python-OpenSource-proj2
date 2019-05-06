0005ea8940fc1299f446821f94a6877b85b339e2
chosungsu <48382347+chosungsu@users.noreply.github.com>
2019-05-06 16:20:30 +0900

    Delete Owner.java

--- Owner.java
+++
@@ -1,6 +0,0 @@
-def Owner(owner):
-    print("This company's owner name is ", owner)
-owner = "JoSungSu"
-Owner(owner)
-caller = str(input("What is your name, sir??"))
-print("===========================================")
