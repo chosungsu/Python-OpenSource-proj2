88ef26829b2a4d1ce6d890c53f7a35d71727cc46
chosungsu <48382347+chosungsu@users.noreply.github.com>
2019-05-06 16:21:27 +0900

    Delete Auto_Callinginroom_System.java

--- Auto_Callinginroom_System.java
+++
@@ -1,5 +0,0 @@
-def Auto_Callinginroom_System(caller, owner):
-    print("Hi,",caller, "Sorry to say you this sad sentences")
-    print(owner, " is calling. Will you call later,please??")
-    print("===========================================")
-Auto_Callinginroom_System(caller, owner)
