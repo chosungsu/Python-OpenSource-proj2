9fce27751cd741fc41413999a0634b4ea31a7cc8
chosungsu <48382347+chosungsu@users.noreply.github.com>
2019-05-06 16:30:06 +0900

    Before issue committed first upload

---
+++ Auto_Callinginroom_System.py
@@ -0,0 +1,6 @@
+def Auto_Callinginroom_System(caller, owner):
+    print("Hi,",caller, "Sorry to say you this sad sentences")
+    print(owner, " is calling. Will you call later,please??")
+    print("===========================================")
+owner = "JoSungSu"
+Auto_Callinginroom_System(caller, owner)
---
+++ Owner.py
@@ -0,0 +1,5 @@
+def Owner(owner):
+    print("This company's owner name is ", owner)
+owner = "JoSungSu"
+Owner(owner)
+print("===========================================")
