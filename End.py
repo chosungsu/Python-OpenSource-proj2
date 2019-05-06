55cd44fabdff73996b3e4fe76555a72ddeedc92f
JoSungSu <ski06043@naver.com>
2019-05-06 19:49:13 +0900

    Closes #6

--- End.py
+++ End.py
@@ -1,3 +1,6 @@
-def End():
-    print("Ok! See you later, sir~~")
-End()
+def End(caller, loc):
+    print("Ok! See you later, ", caller, "~~")
+    print("When you are free visit our company^^")
+    print("Our company is located in ", loc)
+loc = "No-Won-Gu"
+End(caller, loc)
