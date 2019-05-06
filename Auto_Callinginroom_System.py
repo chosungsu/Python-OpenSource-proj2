2a0f5d9938adea2f9ebebe98c2e255256b36fa98
JoSungSu <ski06043@naver.com>
2019-05-06 21:32:13 +0900

    Closes #3

--- Auto_Callinginroom_System.py
+++ Auto_Callinginroom_System.py
@@ -5,7 +5,7 @@ def Auto_Callinginroom_System(caller, owner):
     answer = input()
     if (answer == "y"):
             number = input("번호 입력 : ")
-            ans2 = input("Is this your phone number??(y,n) -> ")
+            ans2 = input("Is this your phone number??(y,n) -> ", number)
             if (ans2 == "y"):
                 print("Ok, Thanks! I will give your number to my boss^^")
             else:
